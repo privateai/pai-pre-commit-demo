724e8ca106d18fb84c60c6496da134880fe35e6a
ketakipai <110412492+ketakipai@users.noreply.github.com>
2022-11-29 20:56:08 +0530

    Dev (#1)
    * added files required to test
    
    * removing ambiguity

---
+++ sample_code.py
@@ -0,0 +1,9 @@
+# Try changing the names here
+names = ["Ryan", "Amy", "Varsha"]
+# And the ages here
+ages = [24, 6, 10]
+
+
+res = dict(zip(names, ages))
+
+print("Resultant dictionary is : " + str(res))
