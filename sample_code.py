01917ee01badab7f39cda9938d2aae883884ebd2
ketakipai <ketaki@private-ai.com>
2022-11-29 16:31:57 +0530

    added files required to test

---
+++ sample_code.py
@@ -0,0 +1,9 @@
+# Try changing the names here
+names = ["Ryan", "Amy", "Varsha"]
+# and / or the ages here
+ages = [24, 6, 10]
+
+
+res = dict(zip(names, ages))
+
+print("Resultant dictionary is : " + str(res))
